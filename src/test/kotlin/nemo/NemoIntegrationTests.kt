c4214eaff8a37b7fabfab6f312231c20211643c4
김용건 (Kim Younggeon) <git+commit@younggeon.kim>
2024-11-29 19:37:44 +0900

    test: contextLoad 테스트 조건 추가

--- src/test/kotlin/nemo/NemoIntegrationTests.kt
+++ src/test/kotlin/nemo/NemoIntegrationTests.kt
@@ -3,14 +3,16 @@ package nemo
 import org.junit.jupiter.api.DisplayName
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.UseMainMethod.ALWAYS
+import org.springframework.context.ApplicationContext
 import kotlin.test.Test
+import kotlin.test.assertNotNull
 
 @SpringBootTest(useMainMethod = ALWAYS)
 internal class NemoIntegrationTests {
 
   @Test
   @DisplayName("어플리케이션은 정상적으로 실행되어야 한다.")
-  fun contextLoads() {
-
+  fun contextLoads(ctx: ApplicationContext) {
+    assertNotNull(ctx.id)
   }
 }
