7433e68368457592ad94187749c4e939f2222cf5
김용건 (Kim Younggeon) <git+commit@younggeon.kim>
2024-11-29 16:13:23 +0900

    feat: 메인 클래스 추가

---
+++ src/main/kotlin/nemo/Nemo.kt
@@ -0,0 +1,14 @@
+package nemo
+
+/**
+ * NEMO 메인 클래스
+ *
+ * @author NEMO maintainers
+ * @since 2024.1.0
+ */
+object Nemo {
+  @JvmStatic
+  fun main(args: Array<String>) {
+    println("Hello, Nemo!")
+  }
+}
---
+++ src/test/kotlin/nemo/NemoTest.kt
@@ -0,0 +1,10 @@
+package nemo
+
+import kotlin.test.Test
+
+internal class NemoTest {
+  @Test
+  fun testMain() {
+    Nemo.main(arrayOf())
+  }
+}
