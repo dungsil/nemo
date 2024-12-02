1a3a68ebc0e99a63d85fae0fd9c5ecb9421116f2
김용건 (Kim Younggeon) <git+commit@younggeon.kim>
2024-12-02 16:00:19 +0900

    chore: Liquibase 추가

--- src/main/kotlin/nemo/Nemo.kt
+++ src/main/kotlin/nemo/Nemo.kt
@@ -2,9 +2,12 @@ package nemo
 
 import nemo.common.logging.createLogger
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
 import org.springframework.context.event.EventListener
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.data.repository.config.BootstrapMode.LAZY
 
 /**
  * NEMO 메인 클래스
@@ -12,6 +15,8 @@ import org.springframework.context.event.EventListener
  * @author NEMO maintainers
  * @since 2024.1.0
  */
+@EntityScan
+@EnableJpaRepositories(bootstrapMode = LAZY) // JPA 저장소 스캔
 @SpringBootApplication
 class Nemo {
   private val log = createLogger {}
