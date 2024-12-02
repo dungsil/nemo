6e6f09d9372c91927fb5d636e3f43a09eb81a0de
☁️dungsil <git+commit@younggeon.kim>
2024-12-02 21:51:25 +0900

    feat: 캐싱 기능 추가 (#7)

---
+++ src/main/kotlin/nemo/common/infrastructure/CacheConfig.kt
@@ -0,0 +1,26 @@
+package nemo.common.infrastructure
+
+import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.caffeine.CaffeineCacheManager
+import org.springframework.context.annotation.AdviceMode.ASPECTJ
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Spring Cache 설정
+ *
+ * @author NEMO maintainers
+ * @since 2024.1.0
+ */
+@Configuration(proxyBeanMethods = false)
+@EnableCaching(mode = ASPECTJ)
+class CacheConfig : CacheManagerCustomizer<CaffeineCacheManager> {
+  override fun customize(cacheManager: CaffeineCacheManager) {
+    cacheManager.apply {
+      isAllowNullValues = false
+
+      // TODO: 외부 구성 속성으로 변경
+      setCacheSpecification("maximumSize=1000,expireAfterWrite=1h")
+    }
+  }
+}
